9c8b93388e4fbb9efb4e37f68ef065fef49d0564
Amrendra Pratap Singh <amrendrapratapcoders@gmail.com>
2018-06-16 21:03:43 +0530

    Arithmetic

---
+++ Arithmetic.py
@@ -0,0 +1,19 @@
+Python 3.7.0b5 (v3.7.0b5:abb8802389, May 31 2018, 01:54:01) [MSC v.1913 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> x = 19
+>>> y =7
+>>> print(x + y)
+26
+>>> print(x-y)
+12
+>>> print(x*y)
+133
+>>> print(x/y)
+2.7142857142857144
+>>> print(x//y)
+2
+>>> print(x%y)
+5
+>>> print(x**y)
+893871739
+>>> 
