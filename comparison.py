be467666983ff066db4f5b0f34ac5060b08a3434
Amrendra Pratap Singh <amrendrapratapcoders@gmail.com>
2018-06-16 21:04:28 +0530

    Comparison

---
+++ comparison.py
@@ -0,0 +1,15 @@
+Python 3.7.0b5 (v3.7.0b5:abb8802389, May 31 2018, 01:54:01) [MSC v.1913 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> x = 19
+>>> y = 7
+>>> print(x>y)
+True
+>>> print(x<y)
+False
+>>> print(x>=y)
+True
+>>> print(x!=y)
+True
+>>> print(x==y)
+False
+>>> 
