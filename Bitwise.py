52d63468cbd0c26d29c8727dd6745e1a0da87ada
Amrendra Pratap Singh <amrendrapratapcoders@gmail.com>
2018-06-16 21:26:39 +0530

    Bitwise Operator

---
+++ Bitwise.py
@@ -0,0 +1,17 @@
+Python 3.7.0b5 (v3.7.0b5:abb8802389, May 31 2018, 01:54:01) [MSC v.1913 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> x = 10
+>>> y = 4
+>>> print(x & y)
+0
+>>> print(x | y)
+14
+>>> print(x ^ y)
+14
+>>> print(~x)
+-11
+>>> print(x>>2)
+2
+>>> print(x<<2)
+40
+>>> 
