9813aff840f8e130d93d36bee9a96a6a514d3a88
Amrendra Pratap Singh <amrendrapratapcoders@gmail.com>
2018-06-16 21:18:00 +0530

    Logical

---
+++ Logical.py
@@ -0,0 +1,13 @@
+Python 3.7.0b5 (v3.7.0b5:abb8802389, May 31 2018, 01:54:01) [MSC v.1913 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> x = True
+>>> y = False
+>>> print(x and y)
+False
+>>> print(x or y)
+True
+>>> print(not x)
+False
+>>> print(not y)
+True
+>>> 
