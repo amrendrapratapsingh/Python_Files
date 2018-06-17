b3d914155fa5faa47af1c10ec4b36ee625feed33
Amrendra Pratap Singh <amrendrapratapcoders@gmail.com>
2018-06-17 14:44:15 +0530

    Casting in Python

---
+++ Casting.py
@@ -0,0 +1,63 @@
+Python 3.7.0b5 (v3.7.0b5:abb8802389, May 31 2018, 01:54:01) [MSC v.1913 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> x =int(5)
+>>> type(x)
+<class 'int'>
+>>> print(x)
+5
+>>> y=7
+>>> print(type(y))
+<class 'int'>
+>>> print(y)
+7
+>>> z =int(8)
+>>> print(z)
+8
+>>> print(type(z))
+<class 'int'>
+>>> x =int(2)
+>>> print(x)
+2
+>>> type(x)
+<class 'int'>
+>>> y =int(3.2)
+>>> print(y)
+3
+>>> type(y)
+<class 'int'>
+>>> z =int("3")
+>>> print(z)
+3
+>>> type(z)
+<class 'int'>
+>>> x =float(3)
+>>> type(x)
+<class 'float'>
+>>> print(x)
+3.0
+>>> y =float(1.3)
+>>> type(y)
+<class 'float'>
+>>> print(y)
+1.3
+>>> z =float("23.2")
+>>> type(z)
+<class 'float'>
+>>> print(z)
+23.2
+>>> x = str(4)
+>>> type(x)
+<class 'str'>
+>>> print(x)
+4
+>>> y =str("3.6")
+>>> type(y)
+<class 'str'>
+>>> print(y)
+3.6
+>>> z = str("Amrendra")
+>>> type(z)
+<class 'str'>
+>>> print(z)
+Amrendra
+>>> 
