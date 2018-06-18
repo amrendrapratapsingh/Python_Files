268089d4972e3e2acf3442394c882dcd01a29fc9
Amrendra Pratap Singh <amrendrapratapcoders@gmail.com>
2018-06-18 16:47:35 +0530

    Condition_if_else_statement

---
+++ Condition_if_else_statement.py
@@ -0,0 +1,8 @@
+print("Begin")
+x=input("Enter a Positive number")
+i=int(x)
+if(i<10):
+    print("Given number is one digit number")
+else:
+        print("Given number is greater than two digit number")
+        print("End")
