c9977624240503a9da2ca5b90561f2fe7ab7853b
Amrendra Pratap Singh <amrendrapratapcoders@gmail.com>
2018-06-19 16:52:49 +0530

    while_loop

---
+++ while_loop1.py
@@ -0,0 +1,6 @@
+print("begin")
+i = 1
+while(i<=5):
+    print("Welcome",i)
+    i = i+1
+print("end")
---
+++ while_loop2.py
@@ -0,0 +1,8 @@
+print("begin")
+i = 1
+while(i<=5):
+    print("Welcome",i)
+    i = i+1
+else:
+    print("In while-else")
+print("End")
